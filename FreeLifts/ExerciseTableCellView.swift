06d41120abbd2dd513072b88449c0f19cb5cfd61
Nick Eubanks <nalexe@live.com>
2016-10-16 19:56:15 -0700

    Add rep countdown buttons for ExerciseTableCellView

--- FreeLifts/ExerciseTableCellView.swift
+++ FreeLifts/ExerciseTableCellView.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ExerciseTableCellView: UITableViewCell {
     @IBOutlet weak var roundedSubView: UIView!
+    @IBOutlet weak var repButton1: UIButton!
+    @IBOutlet weak var repButton2: UIButton!
+    @IBOutlet weak var repButton3: UIButton!
+    @IBOutlet weak var repButton4: UIButton!
+    @IBOutlet weak var repButton5: UIButton!
+    
+    var targetReps = 5; // TODO: Based on data model
     
     override func layoutSubviews() {
         super.layoutSubviews()
         roundedSubView.layer.masksToBounds = true
         roundedSubView.layer.cornerRadius = 10.0
+    }
+    
+    @IBAction func repButtonTouchUp(_ sender: UIButton) {
+        let text = sender.currentTitle
+        if text == nil || text!.isEmpty {
+            // Initial or reset state, set to target rep count
+            sender.setTitle(String(targetReps), for: UIControlState.normal)
+            sender.backgroundColor = #colorLiteral(red: 0, green: 0.3285208941, blue: 0.5748849511, alpha: 1) // TODO: Color from a constant
+        } else if text == "0" {
+            // Final state, reset back to initial
+            sender.setTitle(String(), for: UIControlState.normal)
+            sender.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) // TODO: Color from constant
+        } else {
+            // Decrement reps
+            let repCount = Int(text!)
+            sender.setTitle(String(repCount! - 1), for: UIControlState.normal)
+        }
     }
 }
 
