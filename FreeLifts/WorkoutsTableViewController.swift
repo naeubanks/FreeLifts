5bfaf60684b57d58563fe105c68f7f136287cd5a
Nick Eubanks <nalexe@live.com>
2016-10-17 20:52:35 -0700

    GH: 2 - Initial data model
    Loading participant at launch when opening the workout table view and populating the table with recent workouts

--- FreeLifts/WorkoutsTableViewController.swift
+++ FreeLifts/WorkoutsTableViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import Foundation
 
 class WorkoutsTableViewController: UITableViewController {
     
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,14 +24,30 @@ class WorkoutsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //TODO
-        return 3
+        let workouts = appDelegate.getActiveParticipant().workouts
+        if (workouts == nil) {
+            // One row for starting a new workout
+            return 1;
+        }
+        
+        // Only show the three most recent workouts here plus the current one (or a row to start a new one)
+        return (workouts!.count > 3 ? 3 : 0) + 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let participant = appDelegate.getActiveParticipant()
         let cell = tableView.dequeueReusableCell(withIdentifier: "WorkoutTableCell", for: indexPath)
         let workoutCell = cell as! WorkoutTableCellView
-        workoutCell.labelDate?.text = "TODO"
+    
+        if (indexPath.row == 0) {
+            workoutCell.labelDate?.text = (participant.currentWorkout == nil) ? "Next" : "Ongoing"
+        } else {
+            let workout = participant.workouts?.allObjects[participant.workouts!.count - indexPath.row - 1] as! Workout
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "E, MMM dd"
+            workoutCell.labelDate?.text = dateFormatter.string(from: workout.date as! Date)
+        }
+        
         return cell
     }
     
